28e313d74136e7578b22edef74f037265f296e8b
MatejMeklav <matej.meklav@student.um.si>
2022-05-16 19:53:56 +0200

    Profile Location Add Map added  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/Assets/Styles/Profile.css 	modified:   src/Assets/Styles/SignInUp.css 	modified:   src/Assets/Styles/location.css 	new file:   src/Layouts/Images/location.png 	new file:   src/Layouts/Images/location_no_bg.png 	modified:   src/Layouts/Location/DeleteLocation.tsx 	modified:   src/Layouts/Location/LocationAddLayout.tsx 	deleted:    src/Layouts/Location/LocationPin.tsx 	deleted:    src/Layouts/Location/Map.tsx

--- src/Layouts/Location/DeleteLocation.tsx
+++ src/Layouts/Location/DeleteLocation.tsx
@@ -6,39 +6,49 @@ import { url } from '../../Config/variables';
 export default function DeleteLocation(id: any) {
 
     const [locationId, setLocationId] = useState(id.id);
+    const [deleteLocationSubmited, setDeleteLocationSubmited] = useState(false);
     useEffect(()=>{
         console.log(locationId);
         
-    },[]);
+    },[deleteLocationSubmited]);
 
     const deleteLoc = async () => {
-        console.log("dddddddd");
-        const headers = {
-            'Authorization': 'Bearer '+ localStorage.getItem('key'),
-          };
-        axios.delete(url +'location/delete-location',{data:{locationId}, headers}).then(response => {
-                console.log(response);
-          })
-          .catch(error => {
-            console.log(error.response);
-          });
-          window.location.reload();
+      setDeleteLocationSubmited(true);
+        // console.log("dddddddd");
+        // const headers = {
+        //     'Authorization': 'Bearer '+ localStorage.getItem('key'),
+        //   };
+        // axios.delete(url +'location/delete-location',{data:{locationId}, headers}).then(response => {
+        //         console.log(response);
+        //   })
+        //   .catch(error => {
+        //     console.log(error.response);
+        //   });
+        //   window.location.reload();
     }
 
     const cancel = async () => {
         window.location.reload();
     }
+
+    const submited = <div className='delete-location-submit'>
+                        <p>Your location was deleted.</p>
+                        <button className='submit-btn' onClick={()=> cancel()} type='button'>CLOSE</button>
+                     </div>
+    const notSubmited = <div className='delete-location-div'>
+                          <h4>Are you sure?</h4>
+                          <p>
+                            This location will be deleted.
+                            There is no undo of this action.
+                          </p>
+                          <div className='submit'>
+                            <button className='submit-btn' onClick={() => deleteLoc()} type='button'>SUBMIT</button>
+                            <button className='cancel-btn' onClick={() => cancel()} type='button'>Cancel</button>
+                          </div>
+                        </div>
   return (
-    <div className='delete-location-div'>
-        <h4>Are you sure?</h4>
-        <p>
-            This location will be deleted.
-            There is no undo of this action.
-        </p>
-        <div className='submit'>
-            <button className='submit-btn' onClick={()=>deleteLoc()} type='button'>SUBMIT</button>
-            <button className='cancel-btn' onClick={() => cancel()} type='button'>Cancel</button>
-        </div>
-    </div>
+      <>
+        {deleteLocationSubmited ? submited : notSubmited }
+      </>
   )
 }
--- src/Layouts/Location/LocationAddLayout.tsx
+++ src/Layouts/Location/LocationAddLayout.tsx
@@ -1,34 +1,162 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
 import '../../Assets/Styles/location.css';
-import Map from './Map';
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import marker from '../Images/location_no_bg.png'
+import axios from 'axios';
+import { url } from '../../Config/variables';
 
 export default function LocationAddLayout() {
 
   const [fileInput, setFileInput] = useState('');
+  const [fileToUpload, setFileToUpload] = useState();
   const [fileUploaded, setFileUploaded] = useState(false);
   const [showImage, setShowImage] = useState(false);
+  const [ position, setPosition ] = useState({ latitude: 0, longitude: 0 })
+  const [city, setCity] = useState('')
+  const [town, setTown] = useState('')
+  const [village, setVillage] = useState('')
+  const [country, setCountry] = useState('');
+  const [postNumber, setPostNumber] = useState('');
+  const [municipality, setMunicipality] = useState('');
+  const [fullAddress, setFullAddress] = useState('');
+  const [secureUrl, setSecureUrl] = useState('');
+  const [locationCreated, setLocationCreated] = useState(false);
+
+  const headers = {
+    'Authorization': 'Bearer '+ localStorage.getItem('key'),
+  };
+
+  const LocationMarker =(() => { 
+     const map = useMapEvents({
+       click(e) {
+        const { lat, lng } = e.latlng;
+        setPosition({
+           latitude: lat,
+           longitude: lng,
+         })
+         axios.get('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+lat+'&lon='+lng).then(response => {
+          if(response.data.address.village !== undefined){
+            setVillage(response.data.address.village);
+          }else {
+            setVillage('');
+          }
+          if(response.data.address.town !== undefined){
+            setTown(response.data.address.town);
+          }else {
+              setTown('');
+          }
+          if(response.data.address.city !== undefined){
+            setCity(response.data.address.city);
+          }else {
+            setCity('');
+          }
+          if(response.data.address.municipality !== undefined){
+            setMunicipality(response.data.address.municipality);
+          }else {
+            setMunicipality('');
+          }
+          if(response.data.address.postcode !== undefined){
+            setPostNumber(response.data.address.postcode);
+          }else {
+            setPostNumber('');
+          }
+          setCountry(response.data.address.country)
+          
+        })
+        .catch(error => {
+          console.log(error.response);
+        });   
+        map.flyTo(e.latlng, map.getZoom())
+       },
+       
+     })
+   
+     return (
+         position.latitude !== 0 ? 
+         <Marker 
+           position={[position.latitude, position.longitude]}
+           interactive={false} 
+           icon={myIcon} 
+           />
+   
+          : null
+     )   
+     
+   });
 
   useEffect(()=>{
     if(fileUploaded){
       // @ts-ignore: Object is possibly 'null'.
       let file = (document.getElementById("fileUpload") as HTMLInputElement).files[0];
-      console.log(file);
+       // @ts-ignore: Object is possibly 'null'.
+      setFileToUpload(file);
       setFileInput(URL.createObjectURL(file))
       setShowImage(true);
       setFileUploaded(false);
-      console.log("dddddddddd");
     }
+    if(secureUrl) {
+      axios({
+        method: "PUT",
+        url: secureUrl,
+        data: fileToUpload,  // NOTE - this is the file not the FormData Object
+        headers: { 
+            "Content-Type": "image/png" }
+        }).then(res => {
+          console.log(res);
+       })
+      .catch(err => {
+         console.log(err) 
+      });
+      const imageUrl = secureUrl.split('?')[0];
 
-  },[fileUploaded, showImage]);
+      const {latitude, longitude} = position;
+
+      axios.post(url+"location/create",
+      {
+        image: imageUrl,
+        latitude: latitude,
+        longtitude: longitude,
+        address: fullAddress,
+      },
+    {headers}).then(res => {
+          console.log(res);
+          setLocationCreated(true);
+          setSecureUrl("");
+      })
+      .catch(err => {
+         console.log(err.response) 
+      });
+      setSecureUrl("");
+    }
+    setFullAddress(municipality+" "+city+" "+town+" "+village+" "+postNumber+" "+country);
+
+  },[fileUploaded, showImage, country, postNumber, village, town, city, municipality, secureUrl]);
+  
+  const myIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [32,32]
+   })
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     // @ts-ignore: Object is possibly 'null'.
     let file = (document.getElementById("fileUpload") as HTMLInputElement).files[0];
-    console.log("gggggggggggggggg");
-    
+    if(file === undefined || fullAddress === ''){
+      console.log("nepopolni podatki")
+    }else{
+      axios
+      .get(url + 'secure-url',{headers})
+      .then(response => {
+        setSecureUrl(response.data);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+    } 
     event.preventDefault();
   };
+
   return (
     <div className='location-add'>
       <h4>Add a new <span>location.</span></h4>
@@ -49,17 +177,24 @@ export default function LocationAddLayout() {
             </div>
           </div>
         </div>
-        <div className='google maps'>
-        <Map></Map>
+        <div className='leaflet-map'>
+          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LocationMarker />
+          </MapContainer>
         </div>
+       
         <div className='submit'>
+          <label>Location</label>
+          {locationCreated ? <p id='loc-created'>Location was created sucessfully</p>: "" }
+          <p id='address-value'>{fullAddress}</p>
           <button type='submit' onClick={handleSubmit}>ADD NEW</button>
         </div>
       </form>
       </div>
-
-
-      
     </div>
   )
 }
